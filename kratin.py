4c6c97964657def79fd2d65a8593efb3f60e6a4f
Cvibhansh <93424926+Cvibhansh@users.noreply.github.com>
2021-10-30 19:15:26 +0530

    Update kratin.py

--- kratin.py
+++ kratin.py
@@ -14,8 +14,8 @@ if mydb:
         pass
     else:
         cur.execute("CREATE DATABASE TODOTRACKER")
-        querry="CREATE TABLE Track(trackid integer(10) NOT NULL AUTO_INCREMENT,tdate date NOT NULL,twork varchar(20) NOT NULL,ttime time(0) NOT NULL,tampm varchar(2),PRIMARY KEY (trackid))"
-        cur.execute(querry)
+        create_querry="CREATE TABLE Track(trackid integer(10) NOT NULL AUTO_INCREMENT,tdate date NOT NULL,twork varchar(20) NOT NULL,ttime time(0) NOT NULL,tampm varchar(2),PRIMARY KEY (trackid))"
+        cur.execute(create_querry)
 else:
     print("connection not established !!")
 
@@ -54,19 +54,27 @@ for i in work:
 for i in work:
     os.system("clear")
     print("Set the timer so tha we can alert you for",i)
-    
-    hour=int(input("Enter hour"))
-    time.append(hour)
-    minute=int(input("And what minutes ?"))
-    time.append(minute)
-    amPm=str(input("am or pm?"))
+
+    while 1:
+        time.clear()
+        hour=int(input("Enter hour"))
+        time.append(hour)
+        minute=int(input("And what minutes ?"))
+        time.append(minute)
+        amPm=str(input("am or pm?"))
+
+        reset=input("Are you confirm [yes/no]")
+
+        if reset.lower()=="yes":
+            break
+        
     
     print("waiting for timer",hour,minute,amPm)
     
     if(amPm=="pm"):
         hour=hour+12
         
-    while (1==1):
+    while 1:
         if(hour==datetime.datetime.now().hour and minute==datetime.datetime.now().minute):
             playsound.playsound('beep-02.mp3')
             print("Time to start",i)
@@ -81,8 +89,31 @@ for i in work:
         
 print("plan executed for today")
 while 1:
-    n=input("1 :- press 1 to see you track \n 2 :- exit\n")
+    n=input("1 :- press 1 to see you track\n2 :- see date wise\n3 :- exit\n")
     if n=='1':
         seeTrack()
-    else:
+    elif n=='2':
+     
+        year=input("Enter the year")
+            
+        month=input("enter the month")
+            
+        date=input("enter the date")
+
+        select_query="select * from Track where tdate = '%s'"
+
+        try:
+            cur.execute(select_query,datetime.date(int(year), int(month), int(date)))
+        except  ValueError:
+            print("pls enter proper date")
+        result=cur.fetchall()
+        if result:
+            for x in result:
+                print(x)
+        else:
+            print("No data found")
+    elif n=='3':
         break
+    else:
+        print("pleas select proper option")
+
